1fd30b5d48ebbc1326c319ddbb7e011d28b0e237
Ismail Arai <ismail0311@gmail.com>
2017-07-14 02:02:19 +0900

    change parameters

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ CLIENT-ID=
 
 while :
 do
-	MESSAGE=$ID,13,1,1,1,`date +%Y/%m/%d`,`date +%H:%M:%S`,34.694305,135.269547,13.7,1,0,0,0,1000,32,,,,,,,,0.000196,1,0,0,0,0
+	MESSAGE=$ID,13,1,1,1,`date +%Y/%m/%d`,`date +%H:%M:%S`,34.731894,135.734166,13.7,1,0,0,0,1000,32,,,,,,,,0.000196,1,0,0,0,0
 	$PATH_TO_MQTTSNCLIENT/mqttsnclient -h $HOSTIP -t $TOPIC -c $CLIENT-ID -m $MESSAGE
 	sleep 1
 done
